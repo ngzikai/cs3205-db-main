f208b1e93b141ff0c0490ebd6d110ce4cf12b2b6
AngKiangSiang <ks.dynasty@gmail.com>
2017-09-26 13:16:13 +0800

    SQL for population of default data.

---
+++ Misc/populate_default_values.sql
@@ -0,0 +1,8 @@
+USE CS3205;
+INSERT INTO admin VALUES (default, 'suser', 'password123', 'salty');
+INSERT INTO user VALUES (default, "Bob99", "hello123", "saltysalt", "Bobby", "Mike", "S1234567Z", DATE '2000-12-01', 
+'M', 98989898, 97979797, 96969696, "Kent Ridge", "PGP", "Sentosa Cove", 555555, 544444, 533333, 0, "B+", "123"
+);
+INSERT INTO user VALUES (default, "Alan88", "pass123", "saltysalt", "Alan", "Homie", "S1231231B", DATE '1995-12-29', 
+'M', 999888777, 999777666, 999666555, "Kent Ridge", "PGP", "Sentosa Cove", 543211, 5433333, 546666, 0, "A+", "1234455"
+);
