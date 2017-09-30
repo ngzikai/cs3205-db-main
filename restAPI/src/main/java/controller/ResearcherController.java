42cd1a5a9faee8c9c6a0e8a9cbca3f98882f18e0
Ng Zi Kai <ngzikai92@gmail.com>
2017-10-01 00:03:38 +0800

    Add researcher and delete researcher function in Researcher controller

--- restAPI/src/main/java/controller/ResearcherController.java
+++ restAPI/src/main/java/controller/ResearcherController.java
@@ -3,8 +3,9 @@ package controller;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
+import java.util.Date;
 
 import entity.Researcher;
 import utils.db.MySQLAccess;
@@ -31,14 +32,14 @@ public class ResearcherController {
 				researcher.setFirstname(rs.getString(5));
 				researcher.setLastname(rs.getString(6));
 				researcher.setNric(rs.getString(7));
-				researcher.setDob(rs.getString(8));
+				researcher.setDob(new Date(rs.getDate(8).getTime()));
 				researcher.setGender(rs.getString(9));
 				researcher.setPhone1(rs.getString(10));
 				researcher.setPhone2(rs.getString(11));
 				researcher.setAddress1(rs.getString(12));
 				researcher.setAddress2(rs.getString(13));
-				researcher.setZipcode1(rs.getString(14));
-				researcher.setZipcode2(rs.getString(15));
+				researcher.setZipcode1(rs.getInt(14));
+				researcher.setZipcode2(rs.getInt(15));
 				researcher.setQualifcation(rs.getString(16));
 				researcher.setQualification_name(rs.getString(17));
 			}
@@ -48,7 +49,7 @@ public class ResearcherController {
 		}
 		
 		if(researcher.getResearcher_id() >= 0) {
-			sql = "SELECT category_id from researcher_category WHERE researcher_id = ? AND approval_status = 'Approved'";
+			sql = "SELECT category_id FROM researcher_category WHERE researcher_id = ? AND approval_status = 'Approved'";
 			ArrayList<Integer> researchCategory = new ArrayList<Integer>();
 			try {
 				PreparedStatement ps = connect.prepareStatement(sql);
@@ -65,8 +66,56 @@ public class ResearcherController {
 			}
 			researcher.setResearchCategory(researchCategory);
 		}
-		
 		return researcher;
-		
 	}
+	
+	public void addResearcher(Researcher researcher) {
+		String sql = "INSERT INTO researcher (researcher_username, password, salt, firstname, lastname, nric, dob, gender,"
+				+ "phone1, phone2, address1, address2, zipcode1, zipcode2, qualification, qualification_name) "
+				+ "VALUES ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
+		
+		Connection connect = MySQLAccess.connectDatabase();
+		
+		try {
+			PreparedStatement ps = connect.prepareStatement(sql);
+			ps.setString(1, researcher.getResearcher_username());
+			ps.setString(2, researcher.getPassword());
+			ps.setString(3, researcher.getSalt());
+			ps.setString(4, researcher.getFirstname());
+			ps.setString(5, researcher.getLastname());
+			ps.setString(6, researcher.getNric());
+			ps.setDate(7, new java.sql.Date(researcher.getDob().getTime()));
+			ps.setString(8, researcher.getGender());
+			ps.setString(9, researcher.getPhone1());
+			ps.setString(10, researcher.getPhone2());
+			ps.setString(11, researcher.getAddress1());
+			ps.setString(12, researcher.getAddress2());
+			ps.setInt(13, researcher.getZipcode1());
+			ps.setInt(14, researcher.getZipcode2());
+			ps.setString(15, researcher.getQualifcation());
+			ps.setString(16, researcher.getQualification_name());
+			
+			MySQLAccess.updateDataBasePS(ps);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteResearcher(int researcher_id) {
+		String sql = "DELETE FROM researcher WHERE resercher_id = ?";
+		
+		Connection connect = MySQLAccess.connectDatabase();
+		
+		try {
+			PreparedStatement ps = connect.prepareStatement(sql);
+			ps.setInt(1, researcher_id);
+			
+			MySQLAccess.updateDataBasePS(ps);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//TODO: UPDATE RESEARCHER
 }
