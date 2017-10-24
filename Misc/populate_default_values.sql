128f2c3e1173f9467135f931fc643cae8d73dac1
AngKiangSiang <ks.dynasty@gmail.com>
2017-10-24 23:44:25 +0800

    Updated default values for team1

--- Misc/populate_default_values.sql
+++ Misc/populate_default_values.sql
@@ -1,8 +1,12 @@
 USE CS3205;
-INSERT INTO admin VALUES (default, 'suser', '$2y$10$NHpcPdotapEtq5t2HR.iV.OVHzV8oCymH9n3Fwhnh0CYP9xGE6oG6', 'salty');
-INSERT INTO user VALUES (default, "Bob99", "$2y$10$rV.EgHEAFwc1NZQTncxdi.HTGK9DNDWkjjQ9cHfDk4aoapDUqhPVm", "saltysalt", "Bobby", "Mike", "S1234567Z", DATE '2000-12-01', 
-'M', 98989898, 97979797, 96969696, "Kent Ridge", "PGP", "Sentosa Cove", 555555, 544444, 533333, 0, "B+", "123", "somesecret"
+INSERT INTO admin VALUES (default, 'suser', '$2y$10$NHpcPdotapEtq5t2HR.iV.OVHzV8oCymH9n3Fwhnh0CYP9xGE6oG6', 'salty', '96bc7880544e66a202b24a58440487a6b7df131a');
+INSERT INTO user VALUES (default, "Bob99", "27d7f741010bc4aa99eb9bb430a3922c1284a33f2bd833b628c66294a0c58fed", "","$2y$10$t9P.gFIa9nXhQSMaIbfOqu","", "Bobby", "Mike", "S1234567Z", DATE '2000-12-01', 
+'M', 98989898, 97979797, 96969696, "Kent Ridge", "PGP", "Sentosa Cove", 555555, 544444, 533333, 0, "B+", "123", "somesecret", "Indian", "Singaporean", 1
 );
-INSERT INTO user VALUES (default, "Alan88", "pass123", "saltysalt", "Alan", "Homie", "S1231231B", DATE '1995-12-29', 
-'M', 999888777, 999777666, 999666555, "Kent Ridge", "PGP", "Sentosa Cove", 543211, 5433333, 546666, 0, "A+", "1234455", "somesecret"
+INSERT INTO user VALUES (default, "Alan88", "27d7f741010bc4aa99eb9bb430a3922c1284a33f2bd833b628c66294a0c58fed", "","$2y$10$t9P.gFIa9nXhQSMaIbfOqu","", "Alan", "Homie", "S1231231B", DATE '1995-12-29', 
+'M', 999888777, 999777666, 999666555, "Kent Ridge", "PGP", "Sentosa Cove", 543211, 5433333, 546666, 1, "A+", "1234455", "somesecret", "Chinese", "Malaysia", 0
 );
+INSERT INTO data VALUES (default, 1, "File", "image", "Image of user 1", " 2017-10-22 12:46:41", " 2017-10-22 12:46:41 ", "1507992509630_161UFavhLQy6cRUDZ2s1pH.jpeg");
+INSERT INTO data VALUES (default, 1, "File", "video", "Galt Video of user 1", " 2017-10-22 12:46:41", " 2017-10-22 12:46:41 ", "vid1.mp4");
+
+INSERT INTO treatment VALUES (default, 1, 2, 0);
